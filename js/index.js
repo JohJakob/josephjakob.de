5a82e7ee066058426f9c068418b78df750013a86
Johannes Jakob <johannes@bullg.it>
2017-09-01 08:38:22 +0200

    :checkered_flag: Fix index.js
    :checkered_flag: Fix index.js for Internet Explorer and Microsoft Edge

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ var feed = new Instafeed({
   }
 })
 
-document.getElementsByClassName("instafeed-loader-disabled")[0].removeAttribute("class");
+if (!(/MSIE 9/i.test(navigator.userAgent)) || !(/MSIE 10/i.test(navigator.userAgent)) || !(/rv:11.0/i.test(navigator.userAgent)) || !(/Edge\/\d./i.test(navigator.userAgent))) {
+  document.getElementsByClassName("instafeed-loader-disabled")[0].removeAttribute("class");
+}
 
 feed.run();
